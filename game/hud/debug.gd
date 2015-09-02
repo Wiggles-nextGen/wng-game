0f6148933b2164958e5657a5f83ee9aa8abf9120
René Lottes <rene.lottes@gmx.de>
2015-09-02 20:52:35 +0200

    simplified graph

--- game/hud/debug.gd
+++ game/hud/debug.gd
@@ -10,55 +10,31 @@
 extends Control
 
 var fps_counter
-
-var graph_max_idx = 100
-
-var fps_arr = []
-var tpf_arr = []
-var draw_fps_offset
-var draw_fps_max_height
-
 var gpu_vram
+
+var fps_graph
+var tpf_graph
+var fixed_graph
 
 
 func _ready():
-	initarr()
 	fps_counter = get_node("VBoxContainer/FPS_counter")
 	gpu_vram = get_node("VBoxContainer/GPU_vram")
+	
+	fps_graph = get_node("VBoxContainer/HBoxContainer/Graph_FPS")
+	tpf_graph = get_node("VBoxContainer/HBoxContainer/Graph_TPF")
+	fixed_graph = get_node("VBoxContainer/HBoxContainer/Graph_FIXED")
+	
 	set_process(true)
 	pass
 
 func _process(delta):
-	fps_counter.set_text("FPS: "+str(Performance.get_monitor(Performance.TIME_FPS))+" / "+str(Performance.get_monitor(Performance.TIME_PROCESS)))
-	gpu_vram.set_text(("VRAM: "+str(Performance.get_monitor(Performance.RENDER_VERTEX_MEM_USED))))
+	fps_graph.call("setText","FPS: "+str(Performance.get_monitor(Performance.TIME_FPS)))
+	fps_graph.call("addData",Performance.get_monitor(Performance.TIME_FPS))
 	
-	addFPS(Performance.get_monitor(Performance.TIME_FPS))
-	addTPF(Performance.get_monitor(Performance.TIME_PROCESS)*100)
+	tpf_graph.call("setText","TPF: "+str(Performance.get_monitor(Performance.TIME_PROCESS)))
+	tpf_graph.call("addData",Performance.get_monitor(Performance.TIME_PROCESS))
 	
-	update()
-	pass
+	fixed_graph.call("setText","fixed: "+str(Performance.get_monitor(Performance.TIME_FIXED_PROCESS)))
+	fixed_graph.call("addData",Performance.get_monitor(Performance.TIME_FIXED_PROCESS))
 
-func _draw():
-	for i in range(1,fps_arr.size()):
-		draw_line(Vector2(i-1,fps_arr[i-1]+draw_fps_offset),Vector2(i,fps_arr[i]+draw_fps_offset),Color(1,1,1),1)
-	for i in range(1,tpf_arr.size()):
-		var x = i + graph_max_idx
-		draw_line(Vector2(x-1,tpf_arr[i-1]+draw_fps_offset),Vector2(x,tpf_arr[i]+draw_fps_offset),Color(1,1,1),1)
-
-func initarr():
-	draw_fps_max_height = get_node("VBoxContainer/Control 2").get_size().height
-	draw_fps_offset = get_node("VBoxContainer/Control 2").get_pos().y + draw_fps_max_height
-	for i in range(graph_max_idx):
-		fps_arr.append(0)
-		tpf_arr.append(0)
-
-func addFPS(fps):
-	fps = -clamp(fps,0,draw_fps_max_height)
-	for i in range(fps_arr.size()-1):
-		fps_arr[i] = fps_arr[i+1]
-	fps_arr[fps_arr.size()-1] = fps
-func addTPF(tpf):
-	tpf = -clamp(tpf,0,draw_fps_max_height)
-	for i in range(tpf_arr.size()-1):
-		tpf_arr[i] = tpf_arr[i+1]
-	tpf_arr[tpf_arr.size()-1] = tpf
---
+++ game/hud/graph.gd
@@ -0,0 +1,49 @@
+###########################
+#
+#
+#
+#
+#
+###########################
+
+
+extends VBoxContainer
+
+var dataarr = []
+export(int) var maxdata = 100
+export(int) var drawheight = 60
+export(float) var scale = 1
+export(String) var text = "Label"
+var maxheight
+var offset
+
+
+func _ready():
+	get_node("Label").set_text(text)
+	get_node("Control").set_size(Vector2(0,drawheight))
+	get_node("Control").set_custom_minimum_size(Vector2(0,drawheight))
+	set_custom_minimum_size(Vector2(maxdata+10,0))
+	
+	_initarr()
+
+func _draw():
+	for i in range(1,dataarr.size()):
+		draw_line(Vector2(i-1,dataarr[i-1]+offset),Vector2(i,dataarr[i]+offset),Color(1,1,1),1)
+
+func addData(p):
+	p = p * scale
+	p = -clamp(p,0,maxheight)
+	for i in range(dataarr.size()-1):
+		dataarr[i] = dataarr[i+1]
+	dataarr[dataarr.size()-1] = p
+	update()
+
+func setText(t):
+	text = t
+	get_node("Label").set_text(text)
+
+func _initarr():
+	maxheight = get_node("Control").get_size().height
+	offset = get_node("Label").get_size().height + maxheight
+	for i in range(maxdata):
+		dataarr.append(0)
