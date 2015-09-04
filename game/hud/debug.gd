1d718f437fe32da7595b640ff41db541b3d1aba0
René Lottes <rene.lottes@gmx.de>
2015-09-04 17:38:51 +0200

    changed layout in debug overlay

--- game/hud/debug.gd
+++ game/hud/debug.gd
@@ -16,15 +16,18 @@ var fps_graph
 var tpf_graph
 var fixed_graph
 
+var con
+
 
 func _ready():
-	fps_counter = get_node("VBoxContainer/FPS_counter")
-	gpu_vram = get_node("VBoxContainer/GPU_vram")
+	con = get_node("VBoxContainer/GridContainer")
 	
-	fps_graph = get_node("VBoxContainer/HBoxContainer/Graph_FPS")
-	tpf_graph = get_node("VBoxContainer/HBoxContainer/Graph_TPF")
-	fixed_graph = get_node("VBoxContainer/HBoxContainer/Graph_FIXED")
+	fps_graph = con.get_node("Graph_FPS")
+	tpf_graph = con.get_node("Graph_TPF")
+	fixed_graph = con.get_node("Graph_FIXED")
 	
+	get_tree().connect("screen_resized",self,"_screen_resized")
+	_screen_resized()
 	set_process(true)
 	pass
 
@@ -38,3 +41,14 @@ func _process(delta):
 	fixed_graph.call("setText","fixed: "+str(Performance.get_monitor(Performance.TIME_FIXED_PROCESS)))
 	fixed_graph.call("addData",Performance.get_monitor(Performance.TIME_FIXED_PROCESS))
 
+func _screen_resized():
+	var t = con.get_children()
+	var w = 0
+	var c = 0
+	for i in range(t.size()):
+		w+=t[i].get_size().width
+		if(w<OS.get_window_size().width):
+			c+=1
+	if(c<=0):
+		c = 1
+	con.set_columns(c)
