0ca32f970d9c0eaafd2d442cc57f2e232062f23f
René Lottes <rene.lottes@gmx.de>
2015-09-20 09:17:00 +0200

    added base script for ui menus

--- game/gui/ui_menu.gd
+++ game/gui/ui_menu.gd
@@ -7,20 +7,17 @@
 ###########################
 
 
-extends Control
+extends "ui_base.gd"
 
-var name
 
 
 func _ready():
-	var arr = get_node("VButtonArray")
+	menus = get_node("menus")
+	arr = get_node("VButtonArray")
 	arr.set_button_text(0,tr("UI_MENU_NEW_GAME"))
 	arr.set_button_text(1,tr("UI_MENU_LOAD_GAME"))
 	arr.set_button_text(2,tr("UI_MENU_NEW_GAME"))
 	arr.set_button_text(3,tr("UI_MENU_SETTINGS"))
 	arr.set_button_text(4,tr("UI_MENU_QUIT"))
 	arr.connect("button_selected",self,"_selected")
-	pass
-
-func _selected(id):
-	print(id)
+	_setUp()
--- game/gui/ui_pause.gd
+++ game/gui/ui_pause.gd
@@ -7,19 +7,16 @@
 ###########################
 
 
-extends Control
+extends "ui_base.gd"
 
-var name
 
 
 func _ready():
-	var arr = get_node("VButtonArray")
+	menus = get_node("menus")
+	arr = get_node("VButtonArray")
 	arr.set_button_text(0,tr("UI_MENU_RESUME_GAME"))
 	arr.set_button_text(1,tr("UI_MENU_SETTINGS"))
 	arr.set_button_text(2,tr("UI_MENU_QUIT"))
 	arr.set_pause_mode(PAUSE_MODE_PROCESS)
 	arr.connect("button_selected",self,"_selected")
-	pass
-
-func _selected(id):
-	print(id)
+	_setUp()
