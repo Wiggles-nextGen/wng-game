bb12a5409fa5cfc22ab46397c7fd54d4b39d8a30
MrJustreborn <rene.lottes@gmx.de>
2022-05-14 16:05:28 +0200

    Some gridmap / navmesh tests

---
+++ game/test/Grid.gd
@@ -0,0 +1,11 @@
+extends NavigationRegion3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	bake_navigation_mesh();
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
