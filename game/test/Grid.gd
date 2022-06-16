a7a35aaab68ef49c6b2c53183d99f4cac5e38baa
MrJustreborn <rene.lottes@gmx.de>
2022-06-16 18:46:20 +0200

    I have no clue what i'm doint - something with scaling...

--- game/test/Grid.gd
+++ game/test/Grid.gd
@@ -8,8 +8,11 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	if Input.is_action_just_pressed("ui_up"):
+		$Camera3D.rotate_z(.1);
 	pass
 
 
 func _on_static_body_3d_input_event(camera, event, position, normal, shape_idx):
 	print(camera, event, position, normal, shape_idx)
+	print(get_viewport().get_mouse_position(), " ", get_tree().root.get_viewport().get_mouse_position())
--- game/test/control.gd
+++ game/test/control.gd
@@ -3,6 +3,7 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	#get_viewport().connect("size_changed", _root_viewport_size_changed)
 	pass # Replace with function body.
 
 
@@ -10,9 +11,13 @@ func _ready():
 func _process(delta):
 	pass
 
-
+var factor = 1.0;
 func _on_button_pressed():
-	if get_tree().root.get_content_scale_factor() > 1.0:
-		get_tree().root.set_content_scale_factor(1.0);
+	if factor < 1.0:
+		#get_tree().root.set_content_scale_factor(1.0);
+		factor = 1.0
 	else:
-		get_tree().root.set_content_scale_factor(2);
+		#get_tree().root.set_content_scale_factor(2);
+		factor = .75;
+	
+	$SubViewportContainer/SubViewport.size = get_viewport().size * factor;
